6b258c3a3fac2d7b96386cce61eb59de99eac919
Sergio Martins <sergio.martins@kdab.com>
2024-09-01 16:02:36 +0100

    chore: Allow to run example without building it

--- run_example.sh
+++ run_example.sh
@@ -1,4 +1,11 @@
-cd examples/default_counter_app && \
-flutter build linux --debug --local-engine-src-path=$FLUTTER_ENGINE_FOLDER/.. --local-engine=host_debug_unopt --local-engine-host=host_debug_unopt && \
-cd ../..
+if [ "$1" == "-n" ]; then
+  NO_BUILD_FLUTTER=1
+fi
+
+if [ -z "$NO_BUILD_FLUTTER" ]; then
+    cd examples/default_counter_app && \
+    flutter build linux --debug --local-engine-src-path=$FLUTTER_ENGINE_FOLDER/.. --local-engine=host_debug_unopt --local-engine-host=host_debug_unopt && \
+    cd ../..
+fi
+
 ./build-dev/qtembedder examples/default_counter_app/
