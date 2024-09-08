674b46fd8c7d68a94302e257cbedc981fb19db60
Sergio Martins <sergio.martins@kdab.com>
2024-09-08 13:44:12 +0100

    feat: Port to QCommandLineParser

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@
 #include <string>
 
 #include <QApplication>
+#include <QCommandLineParser>
 
 using namespace KDAB;
 
@@ -22,13 +23,26 @@ int main(int argc, char **argv)
 {
     qputenv("QT_XCB_GL_INTEGRATION", "xcb_egl");
 
+    QApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+    QApplication::setAttribute(Qt::AA_DontCheckOpenGLContextThreadAffinity); // TODO
     QApplication app(argc, argv);
-    app.setAttribute(Qt::AA_DontCheckOpenGLContextThreadAffinity); // TODO
-    app.setAttribute(Qt::AA_ShareOpenGLContexts);
 
-    Embedder embedder(/*multiWindowMode=*/app.arguments().contains("-m"));
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Qt Embedder example");
+    QCommandLineOption enableMultiWindowOpt = { { "m", "multiwindow" }, "Enable multi-window mode" };
+    parser.addOption(enableMultiWindowOpt);
+    parser.addPositionalArgument("project", "the root of the flutter project directory");
+    parser.addHelpOption();
+    parser.process(app);
 
-    const QString projectPath = app.arguments()[1];
+    if (parser.positionalArguments().isEmpty()) {
+        qInfo().noquote() << parser.helpText() << "\n";
+        return 1;
+    }
+
+    const QString projectPath = parser.positionalArguments().constFirst();
+    Embedder embedder(parser.isSet(enableMultiWindowOpt));
+
     const auto icuPath = std::string(FLUTTER_ICUDTL_DIR) + std::string("/icudtl.dat");
 
     if (!embedder.initFlutter(argc, argv, projectPath.toStdString(), icuPath)) {
