17260d6924ddb85a35d190914c2b59ea3d00b3e8
Sergio Martins <sergio.martins@kdab.com>
2024-09-30 11:52:43 +0100

    fix: Don't build script having macOS hardcoded

--- build_example.sh
+++ build_example.sh
@@ -1,8 +1,10 @@
 if [[ "$(uname)" == "Darwin" ]]; then
     FLUTTER_ARCH="_arm64"
+    FLUTTER_HOST="macos"
 else
     FLUTTER_ARCH=""
+    FLUTTER_HOST="linux"
 fi
 
 cd examples/default_counter_app && \
-flutter build macos --debug --local-engine-src-path=$FLUTTER_ENGINE_FOLDER/.. --local-engine=host_debug_unopt${FLUTTER_ARCH} --local-engine-host=host_debug_unopt${FLUTTER_ARCH}
+flutter build ${FLUTTER_HOST} --debug --local-engine-src-path=$FLUTTER_ENGINE_FOLDER/.. --local-engine=host_debug_unopt${FLUTTER_ARCH} --local-engine-host=host_debug_unopt${FLUTTER_ARCH}
