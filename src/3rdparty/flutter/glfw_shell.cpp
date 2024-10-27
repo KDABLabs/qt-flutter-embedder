f8b562ed371ca41030dd4605e68d0f652d9bb7f2
Sergio Martins <sergio.martins@kdab.com>
2024-10-27 19:34:16 +0000

    chore: Add copyright and license to glfw_shell.cpp
    It's copied from the flutter engine repo

--- src/3rdparty/flutter/glfw_shell.cpp
+++ src/3rdparty/flutter/glfw_shell.cpp
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the src/3rdparty/flutter/LICENSE file.
+
 #include "glfw_shell.h"
 #include "flutter_texture_registrar.h"
 
